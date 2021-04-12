e76b764c4164d0e7b6a9924138f816e241402c74
carloscerruti <cacerruti@hotmail.com>
2021-04-12 15:49:21 -0300

    commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>HelpReserve</Text>
+      <Text>by Software Engeneering PUC Campinas</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title:{
+    fontSize:35
+  }
+});
